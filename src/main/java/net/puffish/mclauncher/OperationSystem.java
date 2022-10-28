a3cc5ef680b6f12d1fa1fcedb1b5e6598c77cdf1
CodeCracked <codecrackedgames@gmail.com>
2022-10-28 12:30:03 -0700

    Added Relauncher Entry Point
    * Relauncher now has a custom entry point that can run code before deferring to KnotClient.main

--- src/main/java/net/puffish/mclauncher/OperationSystem.java
+++ src/main/java/net/puffish/mclauncher/OperationSystem.java
@@ -2,37 +2,31 @@ package net.puffish.mclauncher;
 
 import io.vavr.control.Option;
 
-public enum OperationSystem{
-
-	WINDOWS("windows", ";"),
-	MACOS("osx", ":"),
-	LINUX("linux", ":");
-
-	public static Option<OperationSystem> detect(){
-		String os = System.getProperty("os.name").toLowerCase();
-		if(os.contains("win")){
-			return Option.of(WINDOWS);
-		}else if(os.contains("mac")){
-			return Option.of(MACOS);
-		}else if(os.contains("nix") || os.contains("nux")){
-			return Option.of(LINUX);
-		}
-		return Option.none();
-	}
-
-	private final String name;
-	private final String classpathSeparator;
-
-	OperationSystem(String name, String classpathSeparator){
-		this.name = name;
-		this.classpathSeparator = classpathSeparator;
-	}
-
-	public String getName(){
-		return name;
-	}
-
-	public String getClasspathSeparator(){
-		return classpathSeparator;
-	}
+public enum OperationSystem
+{
+    
+    WINDOWS("windows", ";"),
+    MACOS("osx", ":"),
+    LINUX("linux", ":");
+    
+    public static Option<OperationSystem> detect()
+    {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("win")) return Option.of(WINDOWS);
+        else if (os.contains("mac")) return Option.of(MACOS);
+        else if (os.contains("nix") || os.contains("nux")) return Option.of(LINUX);
+        return Option.none();
+    }
+    
+    private final String name;
+    private final String classpathSeparator;
+    
+    OperationSystem(String name, String classpathSeparator)
+    {
+        this.name = name;
+        this.classpathSeparator = classpathSeparator;
+    }
+    
+    public String getName() { return name; }
+    public String getClasspathSeparator() { return classpathSeparator; }
 }
